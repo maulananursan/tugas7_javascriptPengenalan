8911891bb14a35202a6f172ec2fe45d3d3bfafc1
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-09-18 13:18:38 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,12 @@
+var tinggiBadan = [170,171,172,173,174,175,176,177,178,179,180]
+
+console.log("Menggunakan perulangan For")
+for (var i = 0; i < tinggiBadan.length; i++) {
+	console.log(tinggiBadan[i])
+}
+
+console.log("Menggunakan perulangan For of")
+for (let tampil of tinggiBadan){
+	console.log(tampil)
+}
+
